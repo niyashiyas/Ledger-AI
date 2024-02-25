5863f55594772117910b304243901e54529a3fb3
Niya Shiyas <niyashiyas03@gmail.com>
2024-02-25 06:20:11 +0530

    ui

--- client/app/dashboard/page.js
+++ client/app/dashboard/page.js
@@ -1,10 +1,15 @@
-
 "use client"
-import Navbar from '@/components/navbar/Navbar'
-import React from 'react'
+import Image from "next/image";
+import { useRouter } from "next/navigation"
+import {Navbar} from "../../components/navbar/Navbar"
 
-export default function login() {
+
+export default function Home() {
+  const router = useRouter()
+
   return (
-   <Navbar />
-  )
+    <main className="flex flex-col h-screen bg-[#b9bef5] text-black items-center justify-between p-24 py-0">
+      <Navbar/>
+    </main>
+  );
 }
--- client/components/signup/Signup.js
+++ client/components/signup/Signup.js
@@ -28,37 +28,34 @@ export default function Auth() {
 
   return (
     <div className="h-screen">
-      <div className="bg-white h-screen flex flex-col gap-4 items-center justify-center">
+      <div className="bg-[#dee1fa] h-screen flex flex-col gap-4 items-center justify-center">
         <h1 className="text-xl font-bold text-black lg:text-2xl">Sign Up</h1>
-        <p className="-mt-3 text-black">
-          Join Us in Building a Cleaner Tomorrow
-        </p>
         <input
           label="email"
           placeholder="Email"
-          className="border-2 border-black rounded-md p-1 px-2 text-black w-1/6"
+          className=" rounded-md p-1 px-2 text-black w-1/6"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         ></input>
         <input
           label="password"
           placeholder="Password"
-          className="border-2 border-black rounded-md p-1 px-2 text-black w-1/6"
+          className=" rounded-md p-1 px-2 text-black w-1/6"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button
-          className="border-2 border-blue-500 bg-[#009743] rounded-lg p-1 px-2 w-1/6 hover:bg-[#2a724a] hover:text-white"
-          onClick={() => { }}
+          className="border-2 border-balck rounded-lg p-1 px-4 bg-gradient-to-tr from-[#4e5587] to-gray-800 text-white hover:text-slate-200 w-1/6"
+          onClick={() => {}}
         >
           Sign Up
         </button>
         <p
           className="text-gray-700 cursor-pointer"
-          onClick={() => router.push("/login")}
+          onClick={() => router.push("/signin")}
         >
-          Have an account? <span className="text-red-500">Log In.</span>
+          Have an account? <span className="text-blue-700">Log In.</span>
         </p>
       </div>
     </div>
